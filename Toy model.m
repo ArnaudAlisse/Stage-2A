ccfac2a871f48426efb482a03b89351699c1a9f6
ArnaudAlisse <156662570+ArnaudAlisse@users.noreply.github.com>
2025-06-04 14:23:37 +0200

    Create Toy model.m

---
+++ Toy model.m
@@ -0,0 +1,115 @@
+function mpc = toy
+
+%Still some issues, right now the parameters are bullshit so it doens't find a solution with PowerModels, but the syntax is good. 
+%Issues with the DC part, I believe the issue is a lack of informations on the converters "Pancrated" miss and all. 
+
+
+mpc.version = '2';
+mpc.baseMVA = 100.0;
+
+%	bus_i	type	Pd	Qd	Gs	Bs	area	Vm	Va	baseKV	zone	Vmax	Vmin
+mpc.bus = [
+  1	 3	 5	   0.0	 0.0	 0.0	 1	    1.10000	   -0.00000	 240.0	 1	    1.10000	    0.90000;
+	2	 2	 15.0	 2.0	 0.0	 0.0	 1	    0.92617	    7.25883	 240.0	 1	    1.10000	    0.90000;
+	3	 2	 15.0	 0.0	 0.0	 0.0	 1	    0.90000	  -17.26710	 240.0	 2	    1.10000	    0.90000;
+  4	 1	 30.0	 10.0	 0.0	 0.0	 1	    1.10000	   -0.00000	 240.0	 1	    1.10000	    0.90000;
+	5	 1	 20.0	 4.0	 0.0	 0.0	 1	    0.92617	    7.25883	 240.0	 1	    1.10000	    0.90000;
+	6	 1	 25.0	 10.0	 0.0	 0.0	 1	    0.90000	  -17.26710	 240.0	 2	    1.10000	    0.90000;
+  7	 1	 30.0	 0.0	 0.0	 0.0	 1	    1.10000	   -0.00000	 240.0	 1	    1.10000	    0.90000;
+	8	 1	 10.0	 20.0	 0.0	 0.0	 1	    0.92617	    7.25883	 240.0	 1	    1.10000	    0.90000;
+	9	 1	 25.0	 20.0	 0.0	 0.0	 1	    0.90000	  -17.26710	 240.0	 2	    1.10000	    0.90000;
+    
+];
+
+%% generator data
+%	bus	Pg      Qg	Qmax	Qmin	Vg	mBase       status	Pmax	Pmin	pc1 pc2 qlcmin qlcmax qc2min qc2max ramp_agc ramp_10 ramp_30 ramp_q apf
+mpc.gen = [
+	1	 130 -7  48.0	   -30.0	 1.1	     100.0	 1	 150.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0;
+	2	 80  -8	 101.0	 -30.0	 0.92617	 100.0	 1	 260.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0;
+	3	 30  -5  183.0	 -30.0	 0.9	     100.0	 1	 200.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0	 0.0;
+];
+
+mpc.gencost = [
+	2	 1.0	 0.0	 3	   0	   0.01	   0;
+	2	 1.0	 0.0	 3	   0	   0.01    0;
+	2	 1.0	 0.0	 3	   0	   0.01	   0;
+];
+%% branch data
+%	fbus	tbus	r	x	b	rateA	rateB	rateC	ratio	angle	status angmin angmax
+mpc.branch = [
+	  4	 5	 0.042	 0.03	 0.3	 9000.0	 0.0	 0.0	 0.0	 0.0	 1	 -30.0	 30.0;
+   	5	 6   0.042	 0.02	 0.3	 9000.0	 0.0	 0.0	 0.0	 0.0	 1	 -30.0	 30.0;
+    6	 9	 0.042	 0.01	 0.3	 9000.0	 0.0	 0.0	 0.0	 0.0	 1	 -30.0	 30.0;
+    9	 8	 0.042	 0.03	 0.3	 9000.0	 0.0	 0.0	 0.0	 0.0	 1	 -30.0	 30.0;
+    8	 7	 0.042	 0.02	 0.3	 9000.0	 0.0	 0.0	 0.0	 0.0	 1	 -30.0	 30.0;
+    7	 4	 0.042	 0.05	 0.3	 9000.0	 0.0	 0.0	 0.0	 0.0	 1	 -30.0	 30.0;
+];
+
+%I do not know the caracteristics of the cable we may use.
+
+%column_names%	f_bus	t_bus	br_r	br_x	br_b	rate_a	rate_b	rate_c	tap	shift	br_status	angmin	angmax	construction_cost
+mpc.ne_branch = [
+	1	 4	 0.165	 0.012	 0.45	 1000.0	 0.0	 0.0	 0.0	 0.0	 1	 -30.0	 30.0	 1;
+	1	 5   0.025	 0.025	 0.17	 50.0	 0.0	 0.0	 0.0	 0.0	 1	 -30.0	 30.0	 1;
+	1	 6	 0.025	 0.09	 0.27	 500.0	 0.0	 0.0	 0.0	 0.0	 1	 -30.0	 30.0	 1;
+  2	 1	 0.065	 0.062	 0.05	 9000.0	 0.0	 0.0	 0.0	 0.0	 1	 -30.0	 30.0	 1;
+	3	 1	 0.225	 0.005	 0.97	 2000.0	 0.0	 0.0	 0.0	 0.0	 1	 -30.0	 30.0	 1;
+  2	 3	 0.05	 0.35	 0.01	 50.0	 0.0	 0.0	 0.0	 0.0	 1	 -30.0	 30.0    1;
+];
+
+
+%I'm not sure to undestand all that the right way
+
+%% existing dc bus data
+%column_names%   busdc_i grid    Pdc     Vdc     basekVdc    Vdcmax  Vdcmin  Cdc
+mpc.busdc = [
+1              1       0       1       345         1.1     0.9     0;
+2              1       0       1       345         1.1     0.9     0;
+3              1       0       1       345         1.1     0.9     0;
+4              1       0       1       345         1.1     0.9     0;
+5              1       0       1       345         1.1     0.9     0;
+6              1       0       1       345         1.1     0.9     0;
+];
+
+%% existing converters
+%column_names%   busdc_i busac_i type_dc type_ac P_g   Q_g  islcc  Vtar rtf xtf  transformer tm   bf filter    rc      xc  reactor   basekVac Vmmax   Vmmin   Imax    status   LossA LossB  LossCrec LossCinv  droop Pdcset    Vdcset  dVdcset Pacmax Pacmin Qacmax Qacmin
+mpc.convdc = [
+4       4   1       1       -360    -1.66   		0 1.0        0.01  0.01 1 1 0.01 1 0.01   0.01 1  345         1.1     0.9     15     1      1.1033 0.887  2.885    2.885       0.0050    -52.7   1.0079   0 700 -700 700 -700;
+6       6   2       1       -360    -1.66   		0 1.0        0.01  0.01 1 1 0.01 1 0.01   0.01 1  345         1.1     0.9     15     1      1.1033 0.887  2.885    2.885       0.0050    -52.7   1.0079   0 700 -700 700 -700;
+];
+
+
+
+%% candidate dc bus data
+%column_names%   busdc_i grid    Pdc     Vdc     basekVdc    Vdcmax  Vdcmin  Cdc
+mpc.busdc_ne = [
+4              1       0       1       345         1.1     0.9     0;
+6              1       0       1       345         1.1     0.9     0;
+];
+
+%% candidate branches
+%column_names%   fbusdc  tbusdc  r      l        c   rateA   rateB   rateC status cost
+mpc.branchdc_ne = [
+	1	 4	 0.01	 0.00	 0.00  200.0	 0.0	 0.0	 1.0	 2.3;
+	1	 5	 0.01	 0.00	 0.00  200.0	 0.0	 0.0	 1.0	 2.4;
+	1	 6	 0.01	 0.00	 0.00  400.0	 0.0	 0.0	 1.0	 3.4;
+	2	 4	 0.01	 0.00	 0.00  200.0	 0.0	 0.0	 1.0	 2.6;
+	2	 5	 0.01	 0.00	 0.00  400.0	 0.0	 0.0	 1.0	 3.6;
+	2	 6	 0.01	 0.00	 0.00  200.0	 0.0	 0.0	 1.0	 2.7;
+	3	 4	 0.01	 0.00	 0.00  400.0	 0.0	 0.0	 1.0	 3.7;
+	3	 5	 0.01	 0.00	 0.00  200.0	 0.0	 0.0	 1.0	 2.8;
+	3	 6	 0.01	 0.00	 0.00  400.0	 0.0	 0.0	 1.0	 3.8;
+	1	 2	 0.01	 0.00	 0.00  200.0	 0.0	 0.0	 1.0	 2.9;
+	2	 3	 0.01	 0.00	 0.00  400.0	 0.0	 0.0	 1.0	 3.9;
+ ];
+
+%% candidate converters
+%column_names%   busdc_i busac_i type_dc type_ac P_g   Q_g  islcc  Vtar rtf xtf  transformer tm   bf filter    rc      xc  reactor   basekVac Vmmax   Vmmin   Imax    status   LossA LossB  LossCrec LossCinv  droop Pdcset    Vdcset  dVdcset Pacmax Pacmin Qacmax Qacmin cost
+mpc.convdc_ne = [
+1       1   1       1       -360    -1.66   		0 1.0        0.01  0.01 1 1 0.01 1 0.01   0.01 1  345         1.1     0.9     15     1     	1.1033 0.887  2.885    2.885       0.0050    -52.7   1.0079   0 700 -700 700 -700 3;
+2       2   1       1       -360    -1.66   		0 1.0        0.01  0.01 1 1 0.01 1 0.01   0.01 1  345         1.1     0.9     15     1      1.1033 0.887  2.885    2.885       0.0050    -52.7   1.0079   0 700 -700 700 -700 3.5;
+3       3   1       1       -360    -1.66   		0 1.0        0.01  0.01 1 1 0.01 1 0.01   0.01 1  345         1.1     0.9     15     1      1.1033 0.887  2.885    2.885       0.0050    -52.7   1.0079   0 700 -700 700 -700 2.5;
+5       5   1       1       -360    -1.66   		0 1.0        0.01  0.01 1 1 0.01 1 0.01   0.01 1  345         1.1     0.9     15     1      1.1033 0.887  2.885    2.885       0.0050    -52.7   1.0079   0 700 -700 700 -700 4.5;
+];
+
+end
