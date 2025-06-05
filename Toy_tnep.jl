33b81fa7c4ec3bc586ab48a12335c389784f285f
ArnaudAlisse <156662570+ArnaudAlisse@users.noreply.github.com>
2025-06-05 10:19:25 +0200

    Update Toy_tnep.jl
    Corrected a misstake on run_tnepopf

--- Toy_tnep.jl
+++ Toy_tnep.jl
@@ -7,7 +7,6 @@ using Juniper
 using HiGHS
 using HiGHS_jll
 
-#Need to add that because otherwise it's not detected (I have a lot of issues of this type, it's bothering me because I don't succeed in completely solving it)
 
 minlp_solver = JuMP.optimizer_with_attributes(
     Juniper.Optimizer,
@@ -19,20 +18,21 @@ minlp_solver = JuMP.optimizer_with_attributes(
     "log_levels"=>[],
 )
 
-
 network_data = PowerModels.parse_file("C:\\Users\\33781\\OneDrive\\Documents\\STAGE 2A\\toy.m")
-calc_thermal_limits!(network_data) #Allow one to add the thermal limit on the network, may be useful no?
+calc_thermal_limits!(network_data)
 
 
-#result = solve_ac_opf(network_data, Ipopt.Optimizer) #useful to check where are the problems
-result = solve_tnep(network_data, ACPPowerModel, minlp_solver) #solve_tnep call build_tnep, and here is the 1st step to have interesting results
+#result = solve_ac_opf(network_data, Ipopt.Optimizer)
+result = solve_tnep(network_data, ACPPowerModel, minlp_solver) #solve_tnep call build_tnep
+# Afficher les résultats
+println(result["termination_status"])
+println(result["objective"])
+#print_summary(result["solution"])
+#To see which branch has been built : result["solution]["ne_branch"] and check which one has the "built" value at 1 or 0. 1 = built, 0 = not built.
 
-
-#absolutely horrible, so many bugs, took that from a test file and it doens't work aaaaaaah
 
 s = Dict("output" => Dict("branch_flows" => true), "conv_losses_mp" => true)
-#resultDC = result = run_acdctnepopf(network_data, DCPPowerModel, highs ; setting = s)   #The solver used in the test is highs, Ipopt.Optimizer is also a possibilitie
-
+resultDC = run_tnepopf("C:\\Users\\33781\\OneDrive\\Documents\\STAGE 2A\\toy.m"  , DCPPowerModel, minlp_solverres; setting = s) %Again and again it's so useful to look into the test of the source code
 
 #check_tnep_status(result["solution"])
 #check_ne_branch_keys(result["solution"])
@@ -42,11 +42,6 @@ s = Dict("output" => Dict("branch_flows" => true), "conv_losses_mp" => true)
 #I FEEL LIKE I DO NOT USE ALL THE POSSIBILITIES OF POWERMODEL, what I did is like 3 lines, how much more can we do?
 
 
-# Afficher les résultats
-println(result["termination_status"])
-println(result["objective"])
-#print_summary(result["solution"])
 
-#To see which branch has been built : result["ne_branch"] and check which one has the "built" value at 1 or 0. 1 = built, 0 = not built.
 
 
